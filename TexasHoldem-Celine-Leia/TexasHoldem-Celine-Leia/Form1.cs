f6b7fe440a1b93c5eb2fcb042c74b604fd81d9d1
Leia <lksay3d@gmail.com>
2023-02-23 16:30:49 -0500

    Checkpoint 4

--- TexasHoldem-Celine-Leia/TexasHoldem-Celine-Leia/Form1.cs
+++ TexasHoldem-Celine-Leia/TexasHoldem-Celine-Leia/Form1.cs
@@ -40,7 +40,10 @@ namespace TexasHoldem_Celine_Leia
         private void Form1_Load(object sender, EventArgs e)
         {
             player1_C1.Text = game.player1.player1hand[0].ToString();
-              player1_C2.Text = game.player1.player1hand[1].ToString();
+            player1_C2.Text = game.player1.player1hand[1].ToString();
+
+            player2_C1.Text = game.player2.player2hand[0].ToString();
+            player2_C2.Text = game.player2.player2hand[1].ToString();
 
         }
     }
